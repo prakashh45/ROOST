7bd48fc29022954e8fa06c5195af4fccb561424e
prakash45 <prakashpatil0235@gmail.com>
2026-08-22 12:34:17 +0530

    samll changes

--- src/modules/property/property.routes.js
+++ src/modules/property/property.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const propertyController = require("./property.controller");
+const roomController = require("../room/room.controller");
+
 
 const router = express.Router();
 
@@ -12,6 +14,11 @@ router.get(
 router.get(
     "/:slug",
     propertyController.getPropertyBySlug
+);
+
+router.post(
+    "/:propertyId/rooms",
+    roomController.createRoom
 );
 
 router.post("/", propertyController.createProperty);
